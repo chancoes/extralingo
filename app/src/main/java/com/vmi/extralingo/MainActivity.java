d9074c2cc045fc989f97b275b2a1584a97e1f197
Ethan Chanco <110587990+chancoes@users.noreply.github.com>
2024-03-12 21:01:23 -0400

    create registration page and link to sign in page

--- app/src/main/java/com/vmi/extralingo/MainActivity.java
+++ app/src/main/java/com/vmi/extralingo/MainActivity.java
@@ -2,7 +2,14 @@ package com.vmi.extralingo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn_registerPage = findViewById(R.id.btn_RegistrationPage);
+        btn_registerPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, RegistrationActivity.class));
+            }
+        });
     }
+
+
 }
---
+++ app/src/main/java/com/vmi/extralingo/RegistrationActivity.java
@@ -0,0 +1,26 @@
+package com.vmi.extralingo;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+import android.widget.Button;
+
+public class RegistrationActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+
+        Button btn_signInPage = findViewById(R.id.btn_SignInPage);
+        btn_signInPage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(RegistrationActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+}
