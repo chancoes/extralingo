d34e101be239f72986208d27d85bdd3e0f4e4f0f
Ethan Chanco <110587990+chancoes@users.noreply.github.com>
2024-03-14 09:05:33 -0400

    improve layout of lesson overview

--- app/src/main/java/com/vmi/extralingo/LessonOverviewActivity.java
+++ app/src/main/java/com/vmi/extralingo/LessonOverviewActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 public class LessonOverviewActivity extends AppCompatActivity {
@@ -11,6 +12,8 @@ public class LessonOverviewActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lesson_overview);
+
+        ImageButton btn_foodLessonAction = findViewById(R.id.btn_menu_food_lesson);
 
     }
 }
